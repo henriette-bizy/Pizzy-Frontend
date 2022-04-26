6cfb933763b3d6ace3875091c995ad9421f4b9df
henriette-bizy <hopebiziyaremye@gmail.com>
2022-04-26 14:50:33 +0200

    different sign in and sign up page

--- src/form.jsx
+++ src/form.jsx
@@ -2,49 +2,88 @@ import React  from "react";
 
 class PizzyForm extends React.Component{
 
+constructor(props){
+    super(props)
+    this.state = {    
+        isSignUp:false,
+        isSignIn:false
+}
+}
+
+gotoSignIn(){
+ this.setState({isSignIn:true , isSignUp:false})
+}
+
+gotoSignUp(){
+    this.setState({isSignUp:true , isSignIn:false})
+}
+
+    
 render(){
+let Form;
+if(this.state.isSignUp){
+    Form = (<><h2>Sign Up on <span className="">Pizzy</span></h2><form>
+         <div className="row">
+        <label>Names</label>
+        <input type="text" />
+        </div>
+        
+        <div className="row">
+        <label>Email</label>
+        <input type="text" />
+        </div>
+        
+        <div className="row">
+        <label>Password</label>
+        <input type="text" />
+        </div>
+        <div className="row">
+        <label>Confirm Password</label>
+        <input type="text" />
+        </div>
+        
+        <div className="row">
+        <input type="submit" value={"sign In"}/>
+        </div>        
+        </form> </>
+    )
+}
+else{
+
+Form = (<> <h2>Sign In on <span className="">Pizzy</span></h2>
+        <form className="signIn">
+        <div className="row">
+        <label>Email</label>
+        <input type="text" />
+        </div>
+        
+        <div className="row">
+        <label>Password</label>
+        <input type="text" />
+        </div>
+        
+        <div className="row">
+        <input type="submit" value={"sign Up"}/>
+        </div>        
+        </form> 
+    </>)
+
+
+}
 return(
 <div className="form">
 
-    <div className="part1">
-
-
-        
-    </div>
+    <div className="part1"></div>
     <div className="part2">
-    <h2>Sign Up on <span className="">Pizzy</span></h2>
-<form> 
-<div className="row">
-<label>Names</label>
-<input type="text" />
-</div>
-
-<div className="row">
-<label>Email</label>
-<input type="text" />
-</div>
-
-<div className="row">
-<label>Password</label>
-<input type="text" />
-</div>
-<div className="row">
-<label>Confirm Password</label>
-<input type="text" />
-</div>
-
-</form>
+    {Form}
 <div className="form-buttons ">
-<button> Sign up </button>
-<button> Sign in </button>
+<button onClick={()=> {this.gotoSignUp()}}> Sign Up </button>
+<button onClick={()=>{this.gotoSignIn()}}> Sign In </button>
 </div>
 </div>
 </div>
 )
 }
-
-
-
-
 }
+
 export default PizzyForm;
