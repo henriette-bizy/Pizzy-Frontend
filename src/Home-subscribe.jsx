303dd84829b3c0306460f11c43fa5eacbe178b2d
henriette-bizy <hopebiziyaremye@gmail.com>
2022-05-24 21:56:00 +0200

    a few changes on about us page

--- src/Home-subscribe.jsx
+++ src/Home-subscribe.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import {FaArrowCircleRight} from "react-icons/fa"
 class HomeSubscribe extends React.Component{
 
 render(){
@@ -8,13 +9,21 @@ render(){
 
         <div className="subscribe">
             <div className="sub-left">
-  
+           <h2>Join our weekly news later</h2>
+           <div className="sub-input">
+           <input type="text"  placeholder="Your email" />
+           <button>Send</button>
+           </div>
             </div>
 
             <div className="sub-right">
-            <h2>20 May 2022</h2>
-            <p>New meal will arrive on Pizzy</p>
-            <button>Continue</button>
+            <div className="sub-right-profile">
+                <img src={require('./images/Ellipse 3.png')}  />
+            </div>
+            <h2>Martinez Gonzella</h2>
+            <p>" The head director of the coachella amazing pizza restaurant"</p>
+        
+        <FaArrowCircleRight size="32" color="orange" className="sub-right-icon"/>     
             </div>
         </div>
     )
--- src/HomeAbout.jsx
+++ src/HomeAbout.jsx
@@ -9,23 +9,29 @@ render(){
         <div className="About-content">
         <div className="left-about">
         <div className="About-title">
-            <div className="Question">
+                <div className="Question">
         <FaQuestion/>
         </div>
          <h2>About us </h2>
         </div>
-        
+{/*         
                     <p>
            We are Pizzy delivery shop, and as the name suggests it's all
              about Pizza. (because cheese was expensive, we was not rich you know),
               then baked in a pan, stove top style... (mama had no pizza oven).
             <br></br>
             <br></br>
+
+    */}
+    
+
+    <p>
             Well, this tradition is still with our Little Italy Pizza Today!
              Only now we have lots and lots of cheese and a nice big oven. Mangia!
              Among the men who left Southern Italy to find their fortunes making pizza,
               my brother Salvatore has many cherished childhood memories of pizza, made at home
-        </p>
+        </p> 
+        
 
         <div className="about-button">
             <FaPlay className="about-icon"/>
@@ -35,11 +41,15 @@ render(){
 
 
         <div className="right-about">
-         {/* <img src={require("./images/about-1.jpg")} className="about-image" /> */}
-         {/* <img src={require("./images/about-3.jpg")} className="about-image" /> */}
+         <img src={require("./images/combined.png")} className="about-image" />
+         <div className="right-card">
+        
+         </div>
+
+         
         </div>
         </div>
-        </div>
+         </div>
     )
 }
 }
